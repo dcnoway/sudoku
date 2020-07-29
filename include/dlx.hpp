bddf099ab0e4982aab7f2637cec3206ffb8d25c4
Wills Wu <willswu@outlook.com>
2020-07-29 17:50:15 +0800

    init dlx src and unit test

---
+++ include/dlx.hpp
@@ -0,0 +1,555 @@
+/**
+ * @file dlx.hpp
+ * @author Will Wu (willswu@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-07-29
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef __WILLS_SUDOKU_DLX
+#define __WILLS_SUDOKU_DLX
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <unordered_map>
+#include <exception>
+
+using namespace std;
+
+namespace wills::dlx
+{
+    /**
+     * @brief An internal link node class used by DancingLinks
+     * 
+     * @tparam T The typename of the value you wanna stored in the node
+     */
+    template <typename T>
+    class node
+    {
+    public:
+        T val;              //!< value associated to the node
+        size_t col = 0;     //!< column index of the node
+        size_t row = 0;     //!< row index of the node
+        node *p_up = nullptr;   //!< pointer to the up node
+        node *p_down = nullptr; //!< pointer to the down node
+        node *p_left = nullptr; //!< pointer to the left node
+        node *p_right = nullptr;//!< pointer to the right node
+        node *p_col = nullptr;  //!< pointer to the column header
+
+        /**
+         * @brief Construct a new node object with default value
+         */
+        node():val() { p_up = p_down = p_left = p_right = p_col = this; };
+        /**
+         * @brief Construct a new node object
+         * 
+         * @param v the associated value stored in the node
+         */
+        node(T v) : val(v) { p_up = p_down = p_left = p_right = p_col = this; }
+        /**
+         * @brief Copy constructor
+         * 
+         * @param obj Another node instance
+         */
+        node(const node &obj) : val(obj.val)
+        {
+            col = obj.col;
+            row = obj.row;
+            p_up = obj.p_up;
+            p_down = obj.p_down;
+            p_left = obj.p_left;
+            p_right = obj.p_right;
+            p_col = obj.p_col;
+        }
+        /**
+         * @brief copy operator
+         * 
+         * @param obj the right side node instance
+         * @return const node& the left side node instance
+         */
+        const node &operator=(const node &obj)noexcept
+        {
+            val = obj.val;
+            col = obj.col;
+            row = obj.row;
+            p_up = obj.p_up;
+            p_down = obj.p_down;
+            p_left = obj.p_left;
+            p_right = obj.p_right;
+            p_col = obj.p_col;
+            return *this;
+        }
+
+        /**
+         * @brief Simple stream output operator
+         * 
+         * @param os 
+         * @param nd 
+         * @return ostream& 
+         */
+        friend ostream & operator<<(ostream & os,const node & nd){
+            os << "dlx::node{";
+            os << "val=" << nd.val << ", column=" << nd.col <<", row=" << nd.row ;
+            os << ", p_up=" << nd.p_up << ", p_down =" <<nd.p_down ;
+            os << ", p_left=" << nd.p_left <<", p_right=" << nd.p_right ;
+            os << ", p_col=" << nd.p_col << '}';
+            return os;
+        }
+    };
+
+    /**
+     * @brief column header node in the dancing links
+     * 
+     * @tparam T typename of the associated value stored in the dancing links node
+     */
+    template <typename T>
+    class cheader : public node<T>
+    {
+    public:
+        using node<T>::node;    
+        using node<T>::val;
+        using node<T>::p_up;
+        using node<T>::p_down;
+        using node<T>::p_left;
+        using node<T>::p_right;
+        using node<T>::p_col;
+        using node<T>::col;
+        using node<T>::row;
+
+        size_t row_count = 0;   //!< the total sum count of the node on the down side of this column header node
+        
+        /**
+         * @brief Construct a new cheader object
+         * 
+         */
+        cheader()
+        {
+            p_up = this;
+            p_down = this;
+            p_left = this;
+            p_right = this;
+            p_col = this;
+            col = 0;
+            row = 0;
+        }
+        /**
+         * @brief Construct a new cheader object
+         * 
+         * @param v 
+         */
+        cheader(T v)
+        {
+            val = v;
+            p_up = this;
+            p_down = this;
+            p_left = this;
+            p_right = this;
+            p_col = this;
+            col = 0;
+            row = 0;
+        }
+
+        /**
+         * @brief append a node to the last node on the downside of column header
+         *
+         * NOTICE: a node can NOT be append twice! This function never check same node appends. 
+         * @param pnode pointer to the node to be appended
+         * @return cheader* 
+         * - nullptr: failed
+         * - this: succeded
+         */
+        cheader * append(node<T> *pnode)noexcept
+        {
+            if (pnode)
+            {
+                if (pnode->col == col)
+                {
+                    pnode->p_up = p_up;
+                    pnode->p_down = this;
+                    pnode->p_col = this;
+                    p_up->p_down = pnode;
+                    p_up = pnode;
+                    ++row_count;
+                    return this;
+                }
+                else
+                {
+                    cerr << "append a node to cheader with different col index number." << endl;
+                    return nullptr;
+                }
+            }
+            else
+            {
+                cerr << "append a nullptr to a cheader!" << endl;
+                return nullptr;
+            }
+        }
+
+        /**
+         * @brief break the poiter link of the given node from its upper link node and down side link node
+         * 
+         * NOTICE: 
+         * 1. this operation only break the up and down link
+         * 2. this operation DO NOT break the left and right link
+         * 3. adjust the row_count of the column header node
+         * 4. this operation DO NOT delete the node instance
+         * @param pnode pointer to the node which will be removed
+         * @return cheader* 
+         * - nullptr: failed
+         * - this: succeded
+         */
+        cheader * cover_node(node<T> *pnode)noexcept
+        {
+            if(pnode)
+            {
+                for(node<T> * p = p_down;p != this;p=p->p_down){
+                    if(p == pnode){
+                        p->p_up->p_down = p->p_down;
+                        p->p_down->p_up = p->p_up;
+                        --row_count;
+                        return this;
+                        break;
+                    }
+                }
+                return nullptr;
+            }
+            else {
+                cerr << "cover a node with nullptr on a cheader!" << endl;
+                return nullptr;
+            }
+        }
+
+        /**
+         * @brief resume the link of the given node
+         * 
+         * @param pnode the node which will be resumed
+         * @return cheader* 
+         * - nullptr: failed
+         * - this: succeded
+         */
+        cheader * uncover_node(node<T> * pnode)noexcept
+        {
+            if(pnode){
+                if(pnode->col == col){
+                    pnode->p_up->p_down = pnode;
+                    pnode->p_down->p_up = pnode;
+                    ++row_count;
+                    return this;
+                }
+                else {
+                    cerr << "uncover a node not in given col!" << endl;
+                    return nullptr;
+                }
+            }
+            else {
+                cerr << "uncover a node with nullptr on a cheader!" << endl;
+                return nullptr;
+            }
+        }
+
+        /**
+         * @brief the stream output operation of cheader instance
+         * 
+         * @param os 
+         * @param ch 
+         * @return ostream& 
+         */
+        friend ostream & operator<<(ostream & os, const cheader & ch){
+            os << "dlx::cheader{";
+            os << "row_count=" << ch.row_count <<',';
+            os << static_cast<node<T>>(ch) << '}' ;
+            return os;
+        }
+    };
+
+    /**
+     * @brief template alias for std::shared_ptr<cheader<T>>
+     * 
+     * @tparam T typename of the associated value that stored in the node
+     */
+    template<typename T>
+    using pheader_t = typename std::shared_ptr<cheader<T>>;
+
+    /**
+     * @brief template alias for std::shared_ptr<node<T>>
+     * 
+     * @tparam T typename of the associated value that stored in the node
+     */
+    template<typename T>
+    using pnode_t = typename std::shared_ptr<node<T>>;
+
+    /**
+     * @brief helper struct for call dancing_links functions
+     * 
+     * @tparam T typename of the associated value that stored in the node
+     */
+    template<typename T>
+    struct value_t
+    {
+        size_t col; //!< column index
+        T val;      //!< stored value
+    };
+
+    /**
+     * @brief A class implemented classical dancing links algorithm
+     * 
+     * @tparam T typename of the associated value that stored in the node
+     */
+    template<typename T>
+    class dancing_links
+    {
+        /**
+         * @brief the root node of all the dancing link nodes
+         * 
+         * root.row_count is the indicator of the max row index of whole dancing links
+         */
+        cheader<T> root;
+
+        /**
+         * @brief A vector stores all the smart pointer to the column header node
+         * NOTICE: column index start from ONE, but this vector's element index start from ZERO
+         * 
+         */
+        vector<pheader_t<T>> headers;
+
+        /**
+         * @brief A vector holds all nodes's shared_ptr
+         * NOTICE:
+         * 1. No automatic node delete while cover operation, prevent invalid ptr access and segement fault
+         * 2. Auto delete all nodes when dlx obj expired, prevent memory leak
+         */
+        vector<pnode_t<T>> nodes; 
+
+        protected:
+        /**
+         * @brief A helper function to check column header for consistance and linkage
+         * 
+         * @return true status ok
+         * @return false status bad
+         */
+        bool check_cols()noexcept
+        {
+            size_t vector_size = headers.size();
+
+            size_t right_size = 0;
+            for(node<T> * ptr = root.p_right;ptr!=nullptr && ptr != &root;ptr = ptr->p_right)++right_size;
+
+            size_t left_size = 0;
+            for(node<T> * ptr = root.p_left;ptr!=nullptr && ptr != &root; ptr = ptr->p_left)++left_size;
+
+            return ((vector_size == right_size) && (right_size == left_size));
+        }
+
+        public:
+        /**
+         * @brief Construct a new dancing links object
+         * 
+         */
+        dancing_links():root(),headers(),nodes(){};
+
+        /**
+         * @brief stream output function for whole dancing link
+         * 
+         * @param os 
+         * @param dlx 
+         * @return ostream& 
+         */
+        friend ostream & operator << (ostream & os, const dancing_links & dlx){
+            os << "dlx::dancing_links{" <<endl;
+            os << "root=" << dlx.root << endl;
+            os << "column header[" << dlx.nodes.size() <<"]={" << endl;
+            for(auto ele:dlx.headers){
+                os << *ele <<endl;
+            }
+            os << "},";
+            os << "nodes[" << dlx.nodes.size() << "]={" << endl;
+            for(auto it:dlx.nodes){
+                os << it << endl;
+            }
+            os << "}" << endl << '}' << endl;
+            return os;
+        }
+
+        /**
+         * @brief Initilize column header node with given column count
+         * 
+         * @param col_count the total sum of column
+         */
+        void init_cols(size_t col_count){
+            if(headers.size()!=0)throw logic_error("dancing_links::init() on a NOT empty headers!");
+
+            cheader<T> * ptr = &root;
+            for(size_t i = 1 ;i <= col_count; ++i){
+                auto head = make_shared<cheader<T>>(0);
+                headers.push_back(head);
+                head->val = 0;
+                head->col = i;
+                head->p_left = ptr;
+                head->p_right = &root;
+
+                ptr->p_right = head.get();
+                root.p_left = head.get();
+                ptr = head.get();
+            }
+            if(!check_cols())throw logic_error("column header link list error!");
+        }
+
+        /**
+         * @brief clears and delete all the column header node instance
+         * 
+         */
+        void clear_cols()noexcept
+        {
+            if(nodes.size() == 0){
+                headers.clear();
+                root.p_left = &root;
+                root.p_right = &root;
+            }
+        }
+
+        /**
+         * @brief get total amount of column node 
+         * 
+         * @return size_t total amount of column node 
+         */
+        size_t col_size()noexcept
+        {
+            return headers.size();
+        }
+
+        /**
+         * @brief append a row to the end of the dancing links
+         * 
+         * @param values a array contains all column index and the node value.
+         * - a valid column index MUST belongs to [1,col_size()] of the DancingLinks instance.
+         * - the length of values[] MUST belongs to [0,col_size()] of the DancingLinks instance.
+         * - call this function with a blank row(means that a zero length array values), it will do nothing but return the current max row index.
+         * @param values_len the length of values[]
+         * @return size_t the row index of appended row.
+         * - 0       append failed.
+         * - no-zero append succeded and return value is the row index of appended row.
+         */
+        size_t append_row(const value_t<T> values[], const size_t values_len)
+        {
+            //==================================================
+            //Begin paramter validaties check
+            if(values_len > headers.size())
+                throw length_error("append row length exceed col limits.");
+
+            //check the maximum col in the values array
+            for(size_t i =0;i<values_len;++i){
+                if(values[i].col == 0)throw invalid_argument("append a dlx row with a zero col index.");
+                if(values[i].col > headers.size())throw invalid_argument("the maximum col of append row exceed col limits.");
+            }
+
+            //check values array is sorted by col or not
+            //if values array is unsorted, then the link list logic will goes wrong
+            for(size_t i = 1;i<values_len;++i){
+                if(values[i].col<=values[i-1].col)
+                    throw logic_error("call append_row with a unsorted value array.");
+            }
+            //End paramter validaties check
+            //==================================================
+
+            //Blank row append with no effections
+            if(values_len == 0)return root.row_count;
+
+            node<T> * pfirst = nullptr;
+            for(int i =0; i< values_len; ++i){
+                auto v = values [i];
+                pnode_t<T> pnode = std::make_shared<node<T>>(v.val);
+                pnode->col = v.col;
+                pnode->row = root.row_count + 1;
+
+                if(headers[v.col-1]->append(pnode.get())!=headers[v.col-1].get())
+                    throw logic_error("append node error!");
+                else{
+                    //pleft pright
+                    if(nullptr == pfirst){
+                        pfirst = pnode.get();
+
+                    }
+                    pnode->p_left = pfirst->p_left;
+                    pnode->p_right = pfirst;
+
+                    pnode->p_left->p_right = pnode.get();
+                    pnode->p_right->p_left = pnode.get();
+                    //keep smart ptr managed by a vector
+                    nodes.push_back(pnode);
+                }
+            }
+            return ++root.row_count;
+        }
+
+        /**
+         * @brief clear and delete all nodes except root and column header node instance
+         * 
+         */
+        void clear_rows()noexcept
+        {
+            nodes.clear();
+            root.row_count = 0;
+            for(cheader<T> * p = static_cast<cheader<T>*>(root.p_right); 
+                p != & root; 
+                p = static_cast<cheader<T>*>(p->p_right))
+            {
+                p->p_up = p;
+                p->p_down = p;
+                p->row_count =0;
+            }
+        }
+
+        /**
+         * @brief get the total row size of whole dancing links
+         * 
+         * @return size_t total row size
+         */
+        size_t row_size()noexcept
+        {
+            return root.row_count;
+        }
+
+        /**
+         * @brief get the total amount of all the nodes in the dancing links except root and column header node
+         * 
+         * @return size_t total amount of all the nodes 
+         */
+        size_t node_size()noexcept
+        {
+            return nodes.size();
+        }
+        /**
+         * @brief clear and delete all nodes and column header nodes
+         * 
+         */
+        void clear()noexcept
+        {
+            clear_rows();
+            clear_cols();
+        }
+
+        /**
+         * @brief 
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool solve_once(){
+            return false;
+        }
+
+        /**
+         * @brief 
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool solve_multiple(){
+            return false;
+        }
+    };
+} // namespace wills::dlx
+
+#endif
--- test/test1.cpp
+++ test/test1.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
+#include "dlx.hpp"
 
 //Catch2 unit test example
 /*
@@ -11,3 +12,310 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(10) == 3628800 );
 }
 */
+using namespace wills;
+
+TEST_CASE( "dlx::node default constractor check", "[dlx]" ) {
+    dlx::node<int> node;
+    REQUIRE( node.val == 0 );
+    REQUIRE( node.col == 0 );
+    REQUIRE( node.row == 0 );
+    REQUIRE( node.p_col == &node );
+    REQUIRE( node.p_up == &node );
+    REQUIRE( node.p_down == &node );
+    REQUIRE( node.p_left == &node );
+    REQUIRE( node.p_right == &node );
+}
+
+TEST_CASE( "dlx::node copy constractor check", "[dlx]" ) {
+    dlx::node<int> node_a(35);
+    node_a.col = 34;
+    node_a.row = 33;
+
+    dlx::node<int> node_b(node_a);
+    REQUIRE( node_b.val == node_a.val );
+    REQUIRE( node_b.col == node_a.col );
+    REQUIRE( node_b.row == node_a.row );
+    REQUIRE( node_b.p_col == node_a.p_right );
+    REQUIRE( node_b.p_up == node_a.p_up );
+    REQUIRE( node_b.p_down == node_a.p_down );
+    REQUIRE( node_b.p_left == node_a.p_left );
+    REQUIRE( node_b.p_right == node_a.p_right );
+}
+
+TEST_CASE( "dlx::node copy operator check", "[dlx]" ) {
+    dlx::node<int> node_a(35);
+    node_a.col = 34;
+    node_a.row = 33;
+
+    dlx::node<int> node_c(0);
+    node_c = node_a;
+    REQUIRE( node_c.val == node_a.val );
+    REQUIRE( node_c.col == node_a.col );
+    REQUIRE( node_c.row == node_a.row );
+    REQUIRE( node_c.p_col == node_a.p_right );
+    REQUIRE( node_c.p_up == node_a.p_up );
+    REQUIRE( node_c.p_down == node_a.p_down );
+    REQUIRE( node_c.p_left == node_a.p_left );
+    REQUIRE( node_c.p_right == node_a.p_right );
+}
+
+TEST_CASE( "dlx::cheader default constractor check", "[dlx]" ) {
+    dlx::cheader<int> ch;
+    REQUIRE( ch.val == 0 );
+    REQUIRE( ch.col == 0 );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.row_count == 0 );
+
+    dlx::cheader<int> ch2(35);
+    REQUIRE( ch2.val == 35 );
+    REQUIRE( ch2.col == 0 );
+    REQUIRE( ch2.row == 0 );
+    REQUIRE( ch2.row_count == 0 );
+}
+
+TEST_CASE( "dlx::cheader::append check", "[dlx]" ) {
+    dlx::cheader<int> ch;
+    ch.col = 100;
+
+    REQUIRE( ch.append(nullptr) == nullptr );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.row_count == 0 );
+
+    dlx::node<int> node_a(35);
+    node_a.col = 34;
+    node_a.row = 33;
+    REQUIRE( ch.append(&node_a) == nullptr );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.p_down == &ch );
+    REQUIRE( ch.row_count == 0 );
+
+    node_a.col = 0;
+    REQUIRE( ch.append(&node_a) == nullptr );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.row_count == 0 );
+
+    node_a.col = 100;
+    REQUIRE( ch.append(&node_a) == &ch );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.row_count == 1 );
+    REQUIRE( ch.p_up == &node_a );
+    REQUIRE( ch.p_down == &node_a );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &ch );
+
+
+    dlx::node<int> node_b(35);
+    node_b.col = 100;
+    REQUIRE( ch.append(&node_b) == &ch );
+    REQUIRE( node_b.col == ch.col );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.row_count == 2 );
+    REQUIRE( ch.p_up == &node_b );
+    REQUIRE( ch.p_down == &node_a );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &node_b );
+    REQUIRE( node_b.p_up == &node_a );
+    REQUIRE( node_b.p_down == &ch );
+}
+
+TEST_CASE( "dlx::cheader::cover_node check", "[dlx]" ) {
+    //begin prepare test
+    dlx::cheader<int> ch;
+    ch.col = 100;
+    dlx::node<int> node_a(35);
+    node_a.col = 100;
+    REQUIRE( ch.append(&node_a) != nullptr);
+    REQUIRE( ch.row_count == 1 );
+    dlx::node<int> node_b(53);
+    node_b.col = 100;
+    REQUIRE( ch.append(&node_b) != nullptr);
+    REQUIRE( ch.row_count == 2 );
+    //end prepare test
+
+    //check nullptr input
+    REQUIRE( ch.cover_node(nullptr) == nullptr );
+
+    //check wrong col node cover
+    dlx::node<int> node_w1(53);
+    node_w1.col = 10;
+    REQUIRE( ch.cover_node(&node_w1) == nullptr );
+
+    //check cover middle node first
+    REQUIRE( ch.cover_node(&node_a) == &ch );
+    REQUIRE( ch.row_count == 1 );
+    REQUIRE( ch.p_up == &node_b );
+    REQUIRE( ch.p_down == &node_b );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &node_b );
+    REQUIRE( node_b.p_up == &ch );
+    REQUIRE( node_b.p_down == &ch );
+    //check cover next node
+    REQUIRE( ch.cover_node(&node_b) == &ch );
+    REQUIRE( ch.row_count == 0 );
+    REQUIRE( ch.p_up == &ch );
+    REQUIRE( ch.p_down == &ch );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &node_b );
+    REQUIRE( node_b.p_up == &ch );
+    REQUIRE( node_b.p_down == &ch );
+
+    REQUIRE( ch.cover_node(&node_b) == nullptr );
+    REQUIRE( ch.row_count == 0 );
+    REQUIRE( ch.p_up == &ch );
+    REQUIRE( ch.p_down == &ch );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &node_b );
+    REQUIRE( node_b.p_up == &ch );
+    REQUIRE( node_b.p_down == &ch );
+}
+
+TEST_CASE( "dlx::cheader::uncover_node check", "[dlx]" ) {
+    //begin prepare test
+    dlx::cheader<int> ch;
+    ch.col = 100;
+    dlx::node<int> node_a(35);
+    node_a.col = 100;
+    REQUIRE( ch.append(&node_a) != nullptr);
+    dlx::node<int> node_b(53);
+    node_b.col = 100;
+    REQUIRE( ch.append(&node_b) != nullptr);
+    //end prepare test
+
+    //check nullptr input
+    REQUIRE( ch.cover_node(nullptr) == nullptr );
+
+    //check cover middle node first
+    REQUIRE( ch.cover_node(&node_a) == &ch );
+    //check cover next node
+    REQUIRE( ch.cover_node(&node_b) == &ch );
+
+    //uncover null node
+    REQUIRE( ch.uncover_node(nullptr) == nullptr);
+    //uncover node with wrong col index
+    node_b.col = 10;
+    REQUIRE( ch.uncover_node(&node_b) == nullptr);
+
+    //start normal uncover
+    node_b.col = 100;
+    REQUIRE( ch.uncover_node(&node_b) == &ch );
+    REQUIRE( ch.row_count == 1);
+    REQUIRE( ch.uncover_node(&node_a) == &ch );
+    REQUIRE( ch.row_count == 2);
+    REQUIRE( node_b.col == ch.col );
+    REQUIRE( ch.row == 0 );
+    REQUIRE( ch.p_up == &node_b );
+    REQUIRE( ch.p_down == &node_a );
+    REQUIRE( node_a.p_up == &ch );
+    REQUIRE( node_a.p_down == &node_b );
+    REQUIRE( node_b.p_up == &node_a );
+    REQUIRE( node_b.p_down == &ch );
+
+}
+
+TEST_CASE( "dlx::dlx::init_cols check", "[dlx]" ) {
+    dlx::dancing_links<unsigned> dlx;
+    REQUIRE_NOTHROW(dlx.init_cols(10));
+    REQUIRE_NOTHROW(cout<<dlx);
+    REQUIRE(dlx.col_size() == 10);
+
+    REQUIRE_NOTHROW(dlx.clear_cols());
+    REQUIRE_NOTHROW(cout<<dlx);
+    REQUIRE(dlx.col_size() == 0);
+}
+
+TEST_CASE( "dlx::dlx::append_row values length boundary check", "[dlx]" ) {
+    dlx::dancing_links<unsigned> dlx;
+    dlx.init_cols(10);
+
+    //Check normal append 
+    REQUIRE(dlx.row_size() == 0);
+    dlx::value_t<unsigned> values1[5]{{1,11},{3,33},{5,55},{7,77},{9,99}};
+    size_t rs1 = 0;
+    REQUIRE_NOTHROW(rs1 = dlx.append_row(values1,5));
+    REQUIRE(rs1>0);
+    REQUIRE(dlx.row_size() == rs1);
+
+    //Check normal append 
+    REQUIRE(dlx.row_size() == 1);
+    dlx::value_t<unsigned> values2[5]{{2,22},{4,44},{6,66},{8,88},{10,1010}};
+    size_t rs2 = 0;
+    REQUIRE_NOTHROW(rs2 = dlx.append_row(values2,5));
+    REQUIRE(rs2>0);
+    REQUIRE(dlx.row_size() == rs2);
+
+    //Check blank row append 
+    REQUIRE(dlx.row_size() == 2);
+    dlx::value_t<unsigned> values3[]{};
+    size_t rs3 = 0;
+    REQUIRE_NOTHROW(rs3 = dlx.append_row(values3,0));
+    REQUIRE(rs3 == 2);
+    REQUIRE(dlx.row_size() == 2);
+
+    //Check full row append
+    REQUIRE(dlx.row_size() == 2);
+    dlx::value_t<unsigned> values4[10]{{1,11},{2,22},{3,33},{4,44},{5,55},{6,66},{7,77},{8,88},{9,99},{10,1010}};
+    size_t rs4 = 0;
+    REQUIRE_NOTHROW(rs4 = dlx.append_row(values4,10));
+    REQUIRE(rs4 == 3);
+    REQUIRE(dlx.row_size() == 3);
+
+    //Check exceed value length append
+    REQUIRE(dlx.row_size() == 3);
+    dlx::value_t<unsigned> values7[11]{{1,11},{2,22},{3,33},{4,44},{5,55},{6,66},{7,77},{8,88},{9,99},{10,1010},{100,100100}};
+    REQUIRE_THROWS(dlx.append_row(values7,11));
+    REQUIRE(dlx.row_size() == 3);
+}
+
+TEST_CASE( "dlx::dlx::append_row values.col boundary check", "[dlx]" ) {
+    dlx::dancing_links<unsigned> dlx;
+    dlx.init_cols(10);
+
+    //Check zero col value append
+    REQUIRE(dlx.row_size() == 0);
+    dlx::value_t<unsigned> values5[1]{{0,255}};
+    REQUIRE_THROWS(dlx.append_row(values5,1));
+    REQUIRE(dlx.row_size() == 0);
+
+    //Check exceed col value append
+    REQUIRE(dlx.row_size() == 0);
+    dlx::value_t<unsigned> values6[10]{{11,1111},{2,22},{3,33},{4,44},{5,55},{6,66},{7,77},{8,88},{9,99},{10,1010}};
+    REQUIRE_THROWS(dlx.append_row(values6,10));
+    REQUIRE(dlx.row_size() == 0);
+
+
+    //check duplicated col append
+    REQUIRE(dlx.row_size() == 0);
+    dlx::value_t<unsigned> values8[6]{{2,22},{2,22},{4,44},{6,66},{8,88},{10,1010}};
+    REQUIRE_THROWS(dlx.append_row(values8,6));
+    REQUIRE(dlx.row_size() == 0);
+    
+    //check unordered values append
+    dlx::value_t<unsigned> values9[6]{{3,33},{2,22},{4,44},{6,66},{8,88},{10,1010}};
+    REQUIRE_THROWS(dlx.append_row(values9,6));
+    REQUIRE(dlx.row_size() == 0);
+}
+
+
+TEST_CASE( "dlx::dlx::clear() check", "[dlx]" ) {
+    dlx::dancing_links<unsigned> dlx;
+    REQUIRE_NOTHROW(dlx.clear());
+    REQUIRE(dlx.col_size() == 0);
+
+    REQUIRE_NOTHROW(dlx.init_cols(10));
+    REQUIRE(dlx.col_size() == 10);
+
+    REQUIRE_NOTHROW(dlx.clear());
+    REQUIRE(dlx.col_size() == 0);
+
+    REQUIRE_NOTHROW(dlx.init_cols(20));
+    REQUIRE(dlx.col_size() == 20);
+
+    REQUIRE_NOTHROW(dlx.clear());
+    REQUIRE(dlx.col_size() == 0);
+
+    REQUIRE_NOTHROW(dlx.init_cols(100));
+    REQUIRE(dlx.col_size() == 100);
+
+    REQUIRE_NOTHROW(dlx.clear());
+    REQUIRE(dlx.col_size() == 0);
+}
