aff6ea6a5053b602324966791892c95836b6a4b1
Wills Wu <willswu@outlook.com>
2020-08-01 11:05:58 +0800

    init solver unit test

--- test/test_solver.cpp
+++ test/test_solver.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
+#include "classic_board.hpp"
 #include "classic_solver.hpp"
 #include "sample_sudoku.hpp"
 
@@ -7,5 +8,14 @@ using namespace wills::sudoku;
 
 TEST_CASE("classic solver check", "[classic_solver]")
 {
-    REQUIRE(false);
+    auto p_board = std::make_shared<classic_board>();
+
+    vector<wills::sudoku::cell_value_t> 
+        v(sudoku1,sudoku1 + sizeof(sudoku1) /sizeof(unsigned));
+    REQUIRE_NOTHROW(p_board->read(v));
+    classic_solver solver;
+    classic_board * p_answer;
+    REQUIRE_NOTHROW(p_answer = dynamic_cast<classic_board *>(solver.solve(p_board).get()));
+    REQUIRE(p_answer != nullptr);
+
 }
