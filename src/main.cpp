16af64f8d710412e1a36d423b6c4fbc860bf5c31
Wills Wu <willswu@outlook.com>
2020-08-03 20:53:09 +0800

    init main using dynamic rule

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "sudoku.hpp"
 #include "sample_sudoku.hpp"
 #include "classic_board.hpp"
-#include "classic_solver.hpp"
+// #include "classic_solver.hpp"
+#include "classic_rule.hpp"
 using namespace std;
 using namespace wills;
 using namespace wills::sudoku;
@@ -12,17 +13,28 @@ using namespace wills::sudoku;
 int main(int argc, char const *argv[])
 {
     // dlx::dancing_links<unsigned> dlx;
-    auto p_board = std::make_shared<classic_board>();
+    // auto p_board = std::make_shared<classic_board>();
 
-    vector<wills::sudoku::cell_value_t> 
+    vector<wills::sudoku::sudoku_cell_t> 
         v(sudoku1,sudoku1 + sizeof(sudoku1) /sizeof(unsigned));
-    p_board->read(v);
-    cout << *p_board;
+    // p_board->read(v);
+    // cout << *p_board;
 
-    classic_solver solver;
-    classic_board * p_answer = dynamic_cast<classic_board *>(solver.solve(p_board).get());
-    if(p_answer != nullptr)
-        cout << * p_answer;
+    wills::sudoku::sudoku solver;
+    solver.load_cells(v);
+
+    solver.add_rule(make_shared<rules::every_cell_must_has_a_number>());
+    solver.add_rule(make_shared<rules::no_duplicated_number_in_col>());
+    solver.add_rule(make_shared<rules::no_duplicated_number_in_row>());
+    solver.add_rule(make_shared<rules::no_duplicated_number_in_region>());
+
+
+    auto answers = solver.solve();
+
+    for(auto b:answers){
+        std::cout << "=================================" << std::endl;
+        std::cout << b <<std::endl;
+    }
 
     return 0;
 }
